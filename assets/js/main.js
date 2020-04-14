24a4142da432456b3497f0301d913f8acd91faac
Andre Agung Purnomo <andrepurnomo04@gmail.com>
2020-04-14 22:25:37 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // Install service worker
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js").then((reg) => {
+    navigator.serviceWorker.register("../../service-worker.js").then((reg) => {
       console.log("Service worker registered.", reg);
     });
   });
